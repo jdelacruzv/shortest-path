83ffa16a0c5cafcfce7a26519acfde4c9a0d4b9a
Jose De La Cruz <jldlcv@gmail.com>
2024-05-09 22:50:33 +0200

    Add target as the third parameter

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 my_graph = {
-	'A': [('B', 3), ('D', 1)],
+    'A': [('B', 3), ('D', 1)],
     'B': [('A', 3), ('C', 4)],
     'C': [('B', 4), ('D', 7)],
     'D': [('A', 1), ('C', 7)]
 }
 
 
-def shortest_path(graph, start):
+def shortest_path(graph, start, target=''):
     unvisited = list(graph)
     distances = {node: 0 if node == start else float('inf') for node in graph}
     paths = {node: [] for node in graph}
@@ -16,13 +16,15 @@ def shortest_path(graph, start):
         current = min(unvisited, key=distances.get)
         for node, distance in graph[current]:
             if distance + distances[current] < distances[node]:
-                if paths[node][-1] == node:
-                    paths[node] = paths[current]
+                if paths[node] and paths[node][-1] == node:
+                    # Se asigna una copia de la ruta del nodo vecino
+                    paths[node] = paths[current][:]
                 else:
                     paths[node].extend(paths[current])
                 paths[node].append(node)
         unvisited.remove(current)
 
     print(f'Unvisited: {unvisited}\nDistances: {distances}')
+
 
 shortest_path(my_graph, 'A')
